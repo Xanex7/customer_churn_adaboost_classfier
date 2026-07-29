cb46cd70862e8da233d907da12da6987ebb04a7c
dhruv564 <160867137+Xanex7@users.noreply.github.com>
2026-07-29 13:38:48 +0530

    Delete backend directory

--- backend/backend/backend/config/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    PORT = int(os.environ.get("PORT", 5000))
-    DEBUG = os.environ.get("FLASK_ENV") == "development"
-    MODEL_PATH = os.path.join(os.path.dirname(__file__), "..", "..", "model_adaboost_classifier.pkl")
